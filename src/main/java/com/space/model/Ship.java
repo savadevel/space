efecfd87f0be59360fc1cc5141c1ed7f84a51154
Savadevel <savadevel@gmail.com>
2021-05-25 12:15:53 +0300

    refactor: avoid unused imports

--- src/main/java/com/space/model/Ship.java
+++ src/main/java/com/space/model/Ship.java
@@ -3,7 +3,13 @@ package com.space.model;
 
 import com.space.CommonUtils;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.sql.Date;
 
 // В проекте должна использоваться сущность Ship
