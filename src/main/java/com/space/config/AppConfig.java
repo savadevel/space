a0ad57558737b47940171a0c680a62a0f26cc0d1
Savadevel <savadevel@gmail.com>
2021-06-01 13:57:44 +0300

    chore: resolve issues of codacy

--- src/main/java/com/space/config/AppConfig.java
+++ src/main/java/com/space/config/AppConfig.java
@@ -2,9 +2,12 @@ package com.space.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -34,7 +37,7 @@ import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.
 @EnableJpaRepositories(basePackages = "com.space.repository")
 public class AppConfig {
     @Value("${database.populate}")
-    Resource populateScript;
+    private Resource populateScript;
 
     private final Environment env;
 
